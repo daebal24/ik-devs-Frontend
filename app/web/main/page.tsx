4c66e4b9a80fe22d9ce9cb6869840e132cc05d2f
ldi <daebal24@gmail.com>
2026-03-20 18:44:08 +0900

    메인페이지 디자인 변경

--- app/web/main/page.tsx
+++ app/web/main/page.tsx
@@ -41,12 +41,11 @@ export default function Page() {
       <ToastContainer position="top-center" autoClose={3000} />
       <div className={styles.gridContainer}>
 
-        <div className={`${styles.card} ${styles.card1}`} onClick={() => router.push("/web/wpage/viewpage/introduction")}>
+        {/* <div className={`${styles.card} ${styles.card1}`} onClick={() => router.push("/web/wpage/viewpage/introduction")}>
           <div className={styles.cardTop}>
             <div className={styles.cardTitle}>소개</div>
             <div className={styles.cardDesc}>소개 및 안내</div>
           </div>
-          {/* <div className={styles.cardArrow}>↗</div> */}
           <img className={styles.cardImage1} src={"/image/userimg.jpg"} />
         </div>
 
@@ -55,7 +54,6 @@ export default function Page() {
             <div className={styles.cardTitle}>업무 요약</div>
             <div className={styles.cardDesc}>진행 중인 작업 현황 확인</div>
           </div>
-          {/* <div className={styles.cardArrow}>↗</div> */}
           <img className={styles.cardImage2} src={"/image/projectsimg.jpg"} />
         </div>
 
@@ -64,7 +62,6 @@ export default function Page() {
             <div className={styles.cardTitle}>문서 목록</div>
             <div className={styles.cardDesc}>웹 사이트<br/> 문서 목록 관리</div>
           </div>
-          {/* <div className={styles.cardArrow}>↗</div> */}
           <img className={styles.cardImage3} src={"/image/documents_img.jpg"} />
         </div>
 
@@ -73,7 +70,6 @@ export default function Page() {
             <div className={styles.cardTitle}>미디어 파일 목록</div>
             <div className={styles.cardDesc}>웹 사이트<br/> 미디어파일 관리</div>
           </div>
-          {/* <div className={styles.cardArrow}>↗</div> */}
           <img className={styles.cardImage4} src={"/image/album_img.png"} />
         </div>
 
@@ -82,11 +78,49 @@ export default function Page() {
             <div className={styles.cardTitle}>테스팅</div>
             <div className={styles.cardDesc}>기능 테스트</div>
           </div>
-          {/* <div className={styles.cardArrow}>↗</div> */}
           <img className={styles.cardImage5} src={"/image/paper_pen_img.png"} />
+        </div> */}
+
+
+
+        <div className={`${styles.card} ${styles.card1}`} onClick={() => router.push("/web/wpage/viewpage/introduction")}>
+          <div className={styles.cardTop}>
+            <div className={styles.cardTitle}>소개</div>
+            <div className={styles.cardDesc}>소개 및 안내</div>
+          </div>
         </div>
 
+        <div className={`${styles.card} ${styles.card2}`} onClick={() => router.push("/web/tasksummary")}>
+          <div className={styles.cardTop}>
+            <div className={styles.cardTitle}>업무 요약</div>
+            <div className={styles.cardDesc}>진행 중인 작업 현황 확인</div>
+          </div>
+        </div>
+
+        <div className={`${styles.card} ${styles.card4}`} onClick={() => router.push("/web/wpage/managepage")}>
+          <div className={styles.cardTop}>
+            <div className={styles.cardTitle}>문서 목록</div>
+            <div className={styles.cardDesc}>웹 사이트<br/> 문서 목록 관리</div>
+          </div>
+        </div>
+
+        <div className={`${styles.card} ${styles.card3}`} onClick={() => router.push("/web/manageimage/viewimagelist")}>
+          <div className={styles.cardTop}>
+            <div className={styles.cardTitle}>미디어 파일 목록</div>
+            <div className={styles.cardDesc}>웹 사이트<br/> 미디어파일 관리</div>
+          </div>
+        </div>
+
+        <div className={`${styles.card} ${styles.card5}`} onClick={() => router.push("/web/wpage/viewpage/test")}>
+          <div className={styles.cardTop}>
+            <div className={styles.cardTitle}>테스트 낙서장</div>
+            <div className={styles.cardDesc}>기능 테스트</div>
+          </div>
+        </div>
+
+
       </div>
+      {/* <div className={styles.cardArrow}>↗</div> */}
     </main>
   );
 }
